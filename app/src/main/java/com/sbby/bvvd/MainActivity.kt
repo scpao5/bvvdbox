fc81a52559fce97910248afd1f8d5cd2fe04f6a9
scpao5 <3638513045@qq.com>
2026-09-17 00:19:44 +0800

    新增战歌『自由钟声』(Baltic Symphony Orchestra - Freedom, Your Bells Harken to Me, 59s/320kbps)

--- app/src/main/java/com/sbby/bvvd/MainActivity.kt
+++ app/src/main/java/com/sbby/bvvd/MainActivity.kt
@@ -103,7 +103,8 @@ class MainActivity : Activity() {
                 Item("斯拉夫雷霆", "斯拉夫雷霆.mp3", true),
                 Item("丹妮丝之歌", "丹妮丝之歌.flac", true),
                 Item("光荣飞行", "光荣飞行.mp3", true),
-                Item("念巴兰尼科夫", "念巴兰尼科夫.mp3", true)
+                Item("念巴兰尼科夫", "念巴兰尼科夫.mp3", true),
+                Item("自由钟声", "自由钟声.mp3", true)
             )
         )
     )
