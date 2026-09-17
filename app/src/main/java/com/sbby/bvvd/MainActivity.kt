7c25da615580e31699869fd9a499021b3b326811
scpao5 <3638513045@qq.com>
2026-09-17 18:37:16 +0800

    新增战歌『红星复仇者恶搞版』(16.4s/256kbps)

--- app/src/main/java/com/sbby/bvvd/MainActivity.kt
+++ app/src/main/java/com/sbby/bvvd/MainActivity.kt
@@ -104,7 +104,8 @@ class MainActivity : Activity() {
                 Item("丹妮丝之歌", "丹妮丝之歌.flac", true),
                 Item("光荣飞行", "光荣飞行.mp3", true),
                 Item("念巴兰尼科夫", "念巴兰尼科夫.mp3", true),
-                Item("自由钟声", "自由钟声.mp3", true)
+                Item("自由钟声", "自由钟声.mp3", true),
+                Item("红星复仇者恶搞版", "红星复仇者恶搞版.mp3", true)
             )
         )
     )
