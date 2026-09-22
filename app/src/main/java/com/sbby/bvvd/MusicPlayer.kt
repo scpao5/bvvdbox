2e672013aa0e497fc4079e607f385eb865e102e0
scpao5 <3638513045@qq.com>
2026-09-22 22:22:15 +0800

    Revert "修复: MediaPlayer 设置 AudioAttributes(USAGE_MEDIA/CONTENT_TYPE_MUSIC), 对齐系统音量"
    This reverts commit ab3df696c9edf01fa0472585650b2ac7765a29a7.

--- app/src/main/java/com/sbby/bvvd/MusicPlayer.kt
+++ app/src/main/java/com/sbby/bvvd/MusicPlayer.kt
@@ -1,7 +1,6 @@
 package com.sbby.bvvd
 
 import android.content.Context
-import android.media.AudioAttributes
 import android.media.MediaPlayer
 
 class MusicPlayer(private val context: Context) {
@@ -20,12 +19,6 @@ class MusicPlayer(private val context: Context) {
         }
         stop()
         val p = MediaPlayer()
-        p.setAudioAttributes(
-            AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_MEDIA)
-                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                .build()
-        )
         try {
             val afd = context.assets.openFd("music/" + file)
             p.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
